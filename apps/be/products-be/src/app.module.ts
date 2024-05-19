41b0e9b769e521bb09cd7c0005c63c0c2be961fe
Leul Wujira <96904152+meraf00@users.noreply.github.com>
2024-05-19 18:53:32 +0300

    fix: delete unnecessary files

--- apps/be/products-be/src/app.module.ts
+++ apps/be/products-be/src/app.module.ts
@@ -1,10 +1,35 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ProductModule } from './product/product.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { getMetadataArgsStorage } from 'typeorm';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule,
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => {
+        const entities = getMetadataArgsStorage()
+          .tables.map((tbl) => tbl.target)
+          .filter((entity) =>
+            entity.toString().toLowerCase().includes('entity'),
+          );
+
+        return {
+          type: 'mongodb',
+          url: configService.get('MONGODB_CONNECTION_STRING'),
+          database: configService.get('MONGODB_DATABASE'),
+          entities: entities,
+          logging: true,
+          autoLoadEntities: true,
+        };
+      },
+    }),
+    ProductModule,
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
